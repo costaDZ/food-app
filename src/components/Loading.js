7bdb86f3dccd2f1f11e3b91cce5538c3c7c5e76e
costaDZ <73201789+costaDZ@users.noreply.github.com>
2021-06-07 22:25:53 +0100

    add loaders

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -9,8 +9,16 @@ function Loading() {
                     <path className="preloader-path" strokeDashoffset="0" d="M29.437 8.114L19.35 2.132c-1.473-.86-3.207-.86-4.68 0L4.153 8.114C2.68 8.974 1.5 10.56 1.5 12.28v11.964c0 1.718 1.22 3.306 2.69 4.165l10.404 5.98c1.47.86 3.362.86 4.834 0l9.97-5.98c1.472-.86 2.102-2.45 2.102-4.168V12.28c0-1.72-.59-3.306-2.063-4.166z"></path>
                 </svg>
             </div> */}
+            <div className="cart-loader">
+                <div className="loader_img1">
+                    <svg className="circular" viewBox="25 25 50 50">
+                        <circle className="path" cx="50" cy="50" r="20"
+                            fill="none" strokeWidth="3" strokeMiterlimit="10" />
+                    </svg>
+                </div>
+            </div>
 
-            <div class="loader seven"></div>
+            {/* <div class="loader seven"></div> */}
         </div>
     )
 }
--- src/pages/Content.js
+++ src/pages/Content.js
@@ -1,14 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import '../styles/categories.css';
 
-// import Slider from '../components/Slider'
-// import SearchField from '../components/SearchField'
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { useGlobalContext } from '../context';
 import NotFound from '../components/NotFound'
-import Loading from '../components/Loading'
 
 
 
@@ -16,14 +13,17 @@ import Loading from '../components/Loading'
 
 function Content() {
 
-    const { page, food, loading, getIngredients, closeSidebar } = useGlobalContext();
+    const { page, food, getIngredients, closeSidebar } = useGlobalContext();
 
-    const history = useHistory();
+    const [loadingCarts, setLoadingCarts] = useState(true);
 
 
-    if (loading) {
-        return <Loading />
-    }
+    useEffect(() => {
+        setLoadingCarts(true);
+        setTimeout(() => {
+            setLoadingCarts(false);
+        }, 1000);
+    }, [food]);
 
     if (food.length === 0) {
         return (
@@ -32,7 +32,19 @@ function Content() {
                 <NotFound />
             </>
         )
+    }
 
+    function CartLoader() {
+        return (
+            <div className="cart-loader" >
+                <div className="loader_img1">
+                    <svg className="circular" viewBox="25 25 50 50">
+                        <circle className="path" cx="50" cy="50" r="20"
+                            fill="none" strokeWidth="2" strokeMiterlimit="10" />
+                    </svg>
+                </div>
+            </div>
+        )
     }
 
 
@@ -45,44 +57,47 @@ function Content() {
                 }
                 <div className="content-items">
                     {
-                        food.map((item, index) => {
-                            const { label, image, calories, cuisineType, dietLabels, ingredients, totalDaily, totalNutrients } = item.recipe;
-                            console.log(dietLabels);
-                            return (
-                                <section className="food-item" key={index}>
-                                    <div className="image">
-                                        <img src={image} alt={label} />
-                                    </div>
 
-                                    <div className="description">
-                                        <h3 className="food-title">{label}</h3>
-                                        <ul className="des-info">
-                                            {cuisineType ? <li><span>Original Contry</span>{` : ${cuisineType[0].toUpperCase()}`}</li> : null}
-                                            {calories ? <li><span>Calories</span> {` :${calories.toFixed(2)} kcal`}</li> : null}
-                                            {dietLabels.length > 0 ? <li><span>Nutrition Quality</span>{` :${dietLabels}`}</li> : null}
-                                        </ul>
+                        loadingCarts ? food.map((item, index) => <section className="food-item" style={{ background: "white", }} key={index}><CartLoader /> </section>) :
 
 
-                                        <div className="item-btns">
-                                            <Link to="/ingredient">
-                                                <button
-                                                    className="ingredient-btn"
-                                                    onClick={() => getIngredients({ image, label, calories, cuisineType, dietLabels, ingredients })}
-                                                >
-                                                    Show Ingredients
-                                        </button>
-                                            </Link>
-                                            <Link to="/nutrition">
-                                                <button
-                                                    className="nutrition-btn"
-                                                    onClick={() => getIngredients({ image, label, totalDaily, totalNutrients, calories, cuisineType, dietLabels })}
-                                                >Nutrition Info</button>
-                                            </Link>
+                            food.map((item, index) => {
+                                const { label, image, calories, cuisineType, dietLabels, ingredients, totalDaily, totalNutrients } = item.recipe;
+                                return (
+                                    <section className="food-item" key={index}>
+                                        <div className="image">
+                                            <img src={image} alt={label} />
                                         </div>
-                                    </div>
-                                </section>
-                            )
-                        })
+
+                                        <div className="description">
+                                            <h3 className="food-title">{label}</h3>
+                                            <ul className="des-info">
+                                                {cuisineType ? <li><span>Original Contry</span>{` : ${cuisineType[0].toUpperCase()}`}</li> : null}
+                                                {calories ? <li><span>Calories</span> {` :${calories.toFixed(2)} kcal`}</li> : null}
+                                                {dietLabels.length > 0 ? <li><span>Nutrition Quality</span>{` :${dietLabels}`}</li> : null}
+                                            </ul>
+
+
+                                            <div className="item-btns">
+                                                <Link to="/ingredient">
+                                                    <button
+                                                        className="ingredient-btn"
+                                                        onClick={() => getIngredients({ image, label, calories, cuisineType, dietLabels, ingredients })}
+                                                    >
+                                                        Show Ingredients
+                                        </button>
+                                                </Link>
+                                                <Link to="/nutrition">
+                                                    <button
+                                                        className="nutrition-btn"
+                                                        onClick={() => getIngredients({ image, label, totalDaily, totalNutrients, calories, cuisineType, dietLabels })}
+                                                    >Nutrition Info</button>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </section>
+                                )
+                            })
                     }
                 </div>
             </section>
@@ -90,4 +105,4 @@ function Content() {
     )
 }
 
-export default Content
+export default Content;
